4cdcfa8635e7aaec4343c8e82c5cf132462c0e44
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2024-04-28 17:49:46 -0400

    chore: add database postgres

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports= {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '19089910',
+    database: 'portalProfessor',
+    define: {
+       timespamps: true,
+       underscored: true,
+       underscoreAll: true,
+    },
+}
